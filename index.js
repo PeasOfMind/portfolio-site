8a1b33391429b2dc726703832a296650d8712ee6
Yeqing Liu <40677349+PeasOfMind@users.noreply.github.com>
2019-04-06 22:42:43 -0400

    added ability to change contact text on hover or focus to specific contact link

--- index.js
+++ index.js
@@ -18,9 +18,36 @@ function setHeights() {
   mainTop = main.offsetTop;
 }
 
+//Update contact text based on which contact link is hovered over
+function changeContactText(linkId) {
+  let contactText = "";
+  if (linkId === "github"){
+    contactText = "Find My Work on Github";
+  } else if (linkId === "linkedin"){
+    contactText = "Connect With Me on LinkedIn";
+  } else if (linkId === "email"){
+    contactText = "Contact Me Via Email";
+  }
+  document.getElementById('contact-text').textContent = contactText;
+}
+
+function resetContactText(){
+  document.getElementById('contact-text').textContent = "Get in Touch";
+}
+
 window.addEventListener('load', () => {
   setYear();
   setHeights();
 });
 
 window.addEventListener('resize', setHeights);
+
+document.getElementById("github").addEventListener("mouseover", () => changeContactText("github"));
+document.getElementById("linkedin").addEventListener("mouseover", () => changeContactText("linkedin"));
+document.getElementById("email").addEventListener("mouseover", () => changeContactText("email"));
+
+document.getElementById("github").addEventListener("focus", () => changeContactText("github"));
+document.getElementById("linkedin").addEventListener("focus", () => changeContactText("linkedin"));
+document.getElementById("email").addEventListener("focus", () => changeContactText("email"));
+
+document.getElementById("contact-list").addEventListener("mouseleave", resetContactText);
